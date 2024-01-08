0f7b25ca3c15b9a45aac851535cb858b4e7473dc
HanselGray <124652238+HanselGray@users.noreply.github.com>
2024-01-08 12:55:26 +0700

    Add files via upload

--- FIFO.cpp
+++ FIFO.cpp
@@ -6,20 +6,31 @@ using namespace std;
 
 const int FRAME_SIZE = 4;
 const int VIRTUAL_ADDRESS_SPACE = 32;
-
+int page_fault_count = 0,n, maximum_no_frame, allocated =0;
 struct Frame {
     bool occupied;
     int page_number;
+    int phyiscal_address;
+
+    // Frame(bool o, int pn, int pa) : occupied(o), page_number(pn), physical_address(pa) {}
 };
 
 struct Process {
     int id;
     int size;
-    vector<int> page_numbers;
+    vector<Frame>page_numbers;
+    queue<int>evict;
 };
 
-int main() {
-    int n, maximum_no_frame;
+void swap_frame(Process *process,page_id,page_id_swap)
+{
+    *process.page_numbers[page_id].occupied = true;
+
+}
+
+
+int main(){
+
 
     cout << "How many processes?\n";
     cin >> n;
@@ -36,9 +47,7 @@ int main() {
 
         cout << "Enter the page numbers for process " << i << ":\n";
         for (int j = 0; j < no_of_pages; ++j) {
-            int page;
-            cin >> page;
-            temp.page_numbers.push_back(page);
+            temp.page_numbers.emplace_back({false,j,-1});
         }
 
         processes[i] = temp;
@@ -46,69 +55,60 @@ int main() {
 
     cout << "Enter the maximum number of frames in physical memory:\n";
     cin >> maximum_no_frame;
+    vector<int>physical_memory(maximum_no_frame);
 
-    vector<Frame> physical_memory(maximum_no_frame, { false, -1 });
 
-    int page_faults = 0;
+    cout << "Number of frame access request:\n";
+    cin >> n;
+    vector<pair<int,int>>request(n);
+    for(int i = 0; i<n;++i){
+        int t1,t2;
+        cin>>t1>>t2;
+        request.emplace_back(make_pair(t1,t2));
+    }
 
-    for (const auto& process : processes) {
-        cout << "\nProcess " << process.id << ":\n";
+    for(int i = 0 ; i<n;++i){
+        bool page_fault = true;
 
-        for (int page : process.page_numbers) {
-            bool page_fault = true;
+        // inputing processes request for a page.
+        int process_id, page_id;
+        cin >>process_id>>page_id;
+        bool page_fault = true;
 
-            for (auto& frame : physical_memory) {
-                if (frame.occupied && frame.page_number == page) {
-                    page_fault = false;  // Page hit
-                    break;
+        //check if page is present inside of memory
+        if(processes[process_id][page_id].occupied = true ){
+            page_fault = false;
+        }
+
+        // Frame allocate in fault case
+            if (page_fault) {
+                ++page_fault_count;
+                for(int j=0;j<physical_memory.size();++i){
+
+                    //if memory have no free frame, must swap
+                    if(allocated == physical_memory.size()){
+                        int evicted_id = proccess[process_id].evict.pop();
+                        swap_frame(*processes[process_id],page_id,evicted_id);
+                        break;
+                    }
+                    // frame is free
+                    else if(physical_memory[i]== '-1'){
+                        processes[process_id].page_numbers[page_id].occupied = true;
+                        processes[process_id].page_numbers[page_id].phyiscal_address = j;
+                        physical_memory[j] = new; // indicate this frame inside memory have data
+                        processes[process_id].evict.push(page_id);
+                        break;
+                    }
+                    cout << "Process "<<process_id<<"Page fault, allocated page to frame: "<<processes[process_id].page_numbers[page_id].phyiscal_address;
                 }
             }
-
-            if (page_fault) {
-                ++page_faults;
-
-                // Replace page using FIFO algorithm
-                auto oldest_frame = min_element(physical_memory.begin(), physical_memory.end(),
-                    [](const Frame& a, const Frame& b) {
-                        return a.page_number < b.page_number;
-                    });
-
-                oldest_frame->page_number = page;
-                oldest_frame->occupied = true;
-
-                cout << "Page " << page << " loaded into physical memory. Page Fault!\n";
-            }
-            else {
-                cout << "Page " << page << " is already in physical memory. No Page Fault.\n";
+            else{
+                ++page_hit;
+                cout << "Process "<<process_id<<"Page hit in: "<<processes[process_id].page_numbers[page_id].phyiscal_address;
             }
         }
-    }
 
     cout << "\nTotal Page Faults: " << page_faults << endl;
-
-    // Test section for process requesting access to a specific page
-    int test_process_id, test_page_number;
-    cout << "\nEnter the process ID for testing:\n";
-    cin >> test_process_id;
-
-    cout << "Enter the page number for testing:\n";
-    cin >> test_page_number;
-
-    // Simulate process access
-    bool page_fault_test = true;
-    for (auto& frame : physical_memory) {
-        if (frame.occupied && frame.page_number == test_page_number) {
-            page_fault_test = false;  // Page hit
-            break;
-        }
-    }
-
-    if (page_fault_test) {
-        cout << "Page " << test_page_number << " not in physical memory. Page Fault!\n";
-    }
-    else {
-        cout << "Page " << test_page_number << " is already in physical memory. No Page Fault.\n";
-    }
 
     return 0;
 }
